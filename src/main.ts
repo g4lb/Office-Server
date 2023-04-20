7ddcb67538ec0279bfe68a221f6d28c36a68a751
galb <galevgi@gmail.com>
2023-04-20 13:44:26 +0300

    initSwagger function

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { INestApplication } from '@nestjs/common';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-
-    const config = new DocumentBuilder()
+function initSwagger(app: INestApplication) {
+  const config = new DocumentBuilder()
     .setTitle('Employee Logs')
     .setDescription('The employee logs API description')
     .setVersion('1.0')
@@ -13,7 +12,13 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
+}
 
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  initSwagger(app);
   await app.listen(5000);
 }
+
 bootstrap();
+

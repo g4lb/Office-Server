b52162a105ee8bb199aea1e3d5b23e4f200f4787
galb <galevgi@gmail.com>
2023-04-20 12:13:57 +0300

    adding project

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+# Use the official Node.js runtime as a parent image
+FROM node:16-alpine
+
+# Set the working directory to /app
+WORKDIR /src
+
+# Copy the package.json and package-lock.json files to the working directory
+COPY package*.json ./
+
+# Install the dependencies
+RUN npm install
+
+# Copy the rest of the application code to the working directory
+COPY . .
+
+# Expose port 5000 for the application
+EXPOSE 5000
+
+# Set the command to run when the container starts
+CMD ["npm", "run", "start"]
---
+++ test/log.e2e-spec.ts
@@ -0,0 +1,60 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { LogsService } from 'src/logs/logs.service';
+import { Logs } from 'src/logs/logs.entity';
+import { Repository } from 'typeorm';
+import { getRepositoryToken } from '@nestjs/typeorm';
+
+describe('AppController (e2e)', () => {
+  let logsService: LogsService;
+  let logsRepository: Repository<Logs>;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        LogsService,
+        {
+          provide: getRepositoryToken(Logs),
+          useClass: Repository,
+        },
+      ],
+    }).compile();
+
+    logsService = module.get<LogsService>(LogsService);
+    logsRepository = module.get<Repository<Logs>>(getRepositoryToken(Logs));
+
+  });
+  
+  describe('logArrival', () => {
+    it('should create a new log', async () => {
+      const log = new Logs();
+      log.employeeId = 'employee1';
+      log.arrival = new Date('2023-04-20T09:00:00');
+      jest.spyOn(logsRepository, 'save').mockResolvedValueOnce(log);
+      const result = await logsService.logArrival(log.employeeId);
+      expect(result).toEqual(log);
+    });
+  });
+
+  it('should throw an error if the log does not exist', async () => {
+    const log = new Logs();
+    log.employeeId = 'employee1';
+    log.departure = new Date('2023-04-20T18:00:00');
+    jest.spyOn(logsRepository, 'findOne').mockResolvedValueOnce(undefined);
+    await expect(logsService.getLogs(log.employeeId)).rejects.toThrowError(
+      `Employee ${log.employeeId} has not logged arrival`,
+    );
+  });
+
+  it('should throw an error if the log has already been updated', async () => {
+    const log = new Logs();
+    log.id = 1;
+    log.employeeId = 'employee1';
+    log.arrival = new Date('2023-04-20T09:00:00');
+    log.departure = new Date('2023-04-20T18:00:00');
+    jest.spyOn(logsRepository, 'findOne').mockResolvedValueOnce(log);
+    await expect(logsService.getLogs(log.employeeId)).rejects.toThrowError(
+      `Employee ${log.employeeId} has already logged departure`,
+    );
+  });
+  
+});
